578457c1617ecb36c50af39a38f7503671314229
Andre Souza <andrenogueirasouza@gmail.com>
2023-07-05 14:39:51 -0400

    2d stuff

--- examples/fourier.jl
+++ examples/fourier.jl
@@ -1,6 +1,6 @@
 using FFTW, GLMakie
 using ReactingTracers
-
+N = 64
 x = nodes(64)
 k  = wavenumbers(64)
 ∂x = im .* k
@@ -16,4 +16,19 @@ fig = Figure()
 ax = Axis(fig[1, 1])
 scatter!(ax, x, real.(dy))
 lines!(ax, x, cos.(x), color = :red)
+display(fig)
+
+x2d = reshape(x, (64, 1))
+y2d = reshape(x, (1, 64))
+∂x2d = reshape(∂x, (64, 1))
+∂y2d = reshape(∂x, (1, 64))
+
+z = sin.(x2d) .* sin.(y2d)
+dz = real.(ifft(∂y2d .* fft(z)))
+
+fig = Figure()
+ax1 = Axis(fig[1, 1])
+heatmap!(ax1, x2d[:], y2d[:], z)
+ax2 = Axis(fig[1, 2])
+heatmap!(ax2, x2d[:], y2d[:], dz)
 display(fig)
