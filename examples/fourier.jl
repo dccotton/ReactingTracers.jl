27ede311e58bf3f3deadd7bb5b6e06b0dd024174
dccotton <deborah.cotton@stx.ox.ac.uk>
2023-07-05 14:29:59 -0400

    add fourier stuff

--- examples/fourier.jl
+++ examples/fourier.jl
@@ -1,2 +1,19 @@
 using FFTW, GLMakie
 using ReactingTracers
+
+x = nodes(64)
+k  = wavenumbers(64)
+∂x = im .* k
+Δ = ∂x .^2
+
+scatter(x, sin.(x))
+
+y = sin.(x)
+ŷ = fft(y)
+dy = ifft(∂x .* ŷ)
+
+fig = Figure()
+ax = Axis(fig[1, 1])
+scatter!(ax, x, real.(dy))
+lines!(ax, x, cos.(x), color = :red)
+display(fig)
