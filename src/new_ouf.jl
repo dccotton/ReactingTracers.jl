42488afe5e4d492911d8f3e167ae0e9c87bde6bd
dccotton <deborah.cotton@stx.ox.ac.uk>
2023-07-13 17:26:04 -0400

    fix timestepping

--- src/new_ouf.jl
+++ src/new_ouf.jl
@@ -26,11 +26,12 @@ function adv_closure(x)
   end
 end
 
+
 varu=0.1   # variance of u
 r=0.2    # damping rate in OE
-kappa=0.01    # "subgrid" kappa
+kappa=0.02    # "subgrid" kappa
 la=0.05    # relaxation to forcing
-dt=1/5250
+dt= 2/(kappa*1024^2)#1/5250
 rfac=sqrt(2*varu*dt*r)
 
 N=10
@@ -54,15 +55,23 @@ k  = wavenumbers(x_length)
 
 adv2 = adv_closure(zeros(x_length, N))
 
+dt= 2/(kappa*1024^2)
+t=0
+tmax=1000
+tpl=1 # plots every timestep  = 1
+t_array = collect(t:dt:tmax)
+for t in (t_array)
+  if rem(t,tpl)==0
+    print(t)
+  end
+end
+
 ox=ones(x_length,1);
 #magnitudes = [0.7, 0.5, 0.1, 0.2, 0.3, 0.4, 0.6]
-magnitudes = [0.1, 0.5]
-divisor = [25, 13, 6, 3, 1] #[1, 3, 6, 13, 25]#, 63, 125]
+magnitudes = [0.7, 0.1, 0.01]
+divisor = [1, 25] #[25, 13, 6, 3, 1] #[1, 3, 6, 13, 25]#, 63, 125]
 for mag in magnitudes
 for div in ProgressBar(divisor)
-    if any(isnan, c)
-      break
-    end
   # initiate velocities
     u=randn(1,N)*varu #returns a (N by 1) array of random numbers drawn from the standard normal distribution.
     ind = findall(u -> abs(u) >= 5, u)
@@ -70,7 +79,9 @@ for div in ProgressBar(divisor)
 
     # initial concentration
     c=x*zeros(1,N) #array of zeros, depth N, width x (i.e. conc at each point in x for each stochastic choice of v)
-
+    if any(isnan, c)
+      break
+    end
     # concentration bias
     #mag = 0.1;
     #Δconc = mag*sin.(2*pi/div*x)
@@ -78,10 +89,9 @@ for div in ProgressBar(divisor)
     Δconc = Δconc*ones(1, 10);
     size(Δconc)
     # plotting parameters
-    t=0
-    tmax=1000
-    tpl=1 # plots every timestep  = 1
-    t_array = collect(t:dt:tmax)
+    t_array = collect(t:dt:tmax);
+    t_indices = round.(Int, collect(1:length(t_array)/tmax:length(t_array)))
+    save_times = t_array[t_indices]+1
 
     fig = Figure()
     ax = Axis(fig[1, 1], xlabel = L"x",
@@ -99,7 +109,7 @@ for div in ProgressBar(divisor)
     dc = copy(c)
 
     for t in ProgressBar(t_array)
-      if rem(t,tpl)==0
+      if minimum(abs.(save_times .- t)) == 0
         if t > 0
           #f2 = Figure()
           #ax = Axis(f2[1, 1])
@@ -107,8 +117,8 @@ for div in ProgressBar(divisor)
           #lines!(ax, x,(1/N)*c*u'[:]) #plots mean (c) and mean (uc) at each x value, u' = (1 x N), c = (N x length(x)) so matrix multiplication to give u'c = (1 x length(x))
           #title(num2str(t));
           #display(f2)
-          cs[:, Int(t)]= mean(c,dims = 2)
-          fs[:, Int(t)]= (1/N)*c*u'
+          cs[:, round(Int, t)]= mean(c,dims = 2)
+          fs[:, round(Int, t)]= (1/N)*c*u'
         end
       end
       #  tmp=c;
@@ -136,9 +146,11 @@ for div in ProgressBar(divisor)
   #title(num2str(t));
   display(f3)
 
-  save_name = "mag_" * string(mag) * "_k_" * string(round(div, sigdigits = 3)) * "_FT.jld2"
+  save_name = "mag_" * string(mag) * "_k_" * string(round(div, sigdigits = 3)) * "_kappa_0.02_FT.jld2"
 
   @save save_name cs fs ff cf gc
 end
 
 end
+
+save_name = "mag_" * string(mag) * "_k_" * string(round(div, sigdigits = 3)) * "_kappa_0.02_FT.jld2"
