a31907803b727361fed07a2317e512a343287775
Andre Souza <andrenogueirasouza@gmail.com>
2023-08-04 11:33:29 -0400

    some more small changes

--- src/ode_solve.jl
+++ src/ode_solve.jl
@@ -8,12 +8,11 @@ x = nodes(x_length, a = -pi, b = pi)
 
 # This is a function of two variables c1(t) and c2(t) as opposed
 # to a PDE of of two variables c¹(x, t), c²(x, t)
-function two_state!(dc, c, p, x)
+function two_state!(dc, c, p, t)
     λ = 1
     mag = 0.7
-    c0  = 1 + mag
-    dc[1] = λ*c[1] - 2*λ*c[1]^2/c0  - c[1]/2 + c[2]/2
-    dc[2] = λ*c[2] - 2*λ*c[2]^2/c0  + c[1]/2 - c[2]/2
+    dc[1] = λ*c[1] - 2*λ*c[1]^2/(1 + mag * cos(p[1]))  - c[1]/2 + c[2]/2
+    dc[2] = λ*c[2] - 2*λ*c[2]^2/(1 + mag * cos(p[2]))  + c[1]/2 - c[2]/2
     #dc[1] = -λ*c[1] .+ c[1]/2 .- c[1]/2
     #dc[2] = λ*c[1] .- c[1]/2 .+ c[1]/2
     return nothing
@@ -22,7 +21,8 @@ end
 c_initial = [0.5, 0.5]
 c = copy(c_initial)
 tspan = (0, 10)
-prob = ODEProblem(two_state!, c_initial, tspan)
+p = (-π, π)
+prob = ODEProblem(two_state!, c_initial, tspan, p)
 solver = RK4()  # Choose a solver (e.g., Tsit5, RK4, etc.)
 sol = solve(prob, solver)
 
